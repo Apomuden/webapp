aaa4113afbe0a041e1c15b5c25c1d2ca7af7d210
Joseph Cobbinah <josephcobbinah.jc@gmail.com>
2020-01-09 20:04:20 +0000

    populated selects of patient creation form

--- src/app/records/records.module.ts
+++ src/app/records/records.module.ts
@@ -1,18 +1,8 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RegisterPatientComponent} from './register-patient/register-patient.component';
-import {RecordsRoutingModule} from './records-routing.module';
-import {
-  NzButtonModule,
-  NzDatePickerModule,
-  NzFormModule,
-  NzGridModule,
-  NzIconModule,
-  NzInputModule, NzInputNumberModule,
-  NzSelectModule,
-  NzStepsModule, NzSwitchModule
-} from 'ng-zorro-antd';
-import {ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterPatientComponent } from './register-patient/register-patient.component';
+import { RecordsRoutingModule } from './records-routing.module';
+
 import { SharedModule } from '../shared/shared.module';
 
 
@@ -24,4 +14,4 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule,
   ]
 })
-export class RecordsModule {}
+export class RecordsModule { }
