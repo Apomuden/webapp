377123a9fb6bd9e67030ffdf19a75e3c1bbdbc12
Wilfred Asomani <devwilfredagyei@gmail.com>
2020-04-28 01:30:33 +0000

    UI improvement create sample code for saving results

--- src/app/laboratory/lab/lab.component.ts
+++ src/app/laboratory/lab/lab.component.ts
@@ -10,6 +10,8 @@ import { PhysicianService } from 'src/app/physician/services/physician.service';
 import { OpdService } from 'src/app/opd/services/opd.service';
 import { LaboratoryService } from '../services/laboratory.service';
 import { LabSetupService } from 'src/app/shared/services/lab-setup.service';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
+import { User } from 'src/app/shared/interfaces/user.type';
 
 @Component({
   selector: 'app-lab',
@@ -22,19 +24,25 @@ export class LabComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild(NzInputDirective, { static: false, read: ElementRef }) inputElement: ElementRef;
 
   folderNumb = this.fb.control(null, [Validators.minLength(11), Validators.maxLength(12)]);
+  sampleTypeCtrl = this.fb.control(null, [Validators.required]);
 
   doctorsModalVisible = false;
   doctorsLoading = false;
   isLoadingData = false;
   searchInitialized = false;
   submiting = false;
+  isCreatingSample = false;
   isAssigning = false;
 
   patient: any;
   message = 'Please enter a valid folder number to fill this form.';
   labRequest: any;
   consultation: any;
-  labParams: any;
+  labParams: any[];
+  sampleTypes: any[];
+  sample: any;
+  user: User;
+  results: any;
 
   constructor(
     private fb: FormBuilder,
@@ -42,6 +50,7 @@ export class LabComponent implements OnInit, OnDestroy, AfterViewInit {
     private labService: LaboratoryService,
     private labSetupService: LabSetupService,
     private physicianService: PhysicianService,
+    private authService: AuthenticationService,
     private notificationS: NzNotificationService,
   ) { }
 
@@ -51,7 +60,9 @@ export class LabComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit() {
-    this.today = formatDate(datefns.startOfToday(), 'yyyy-MM-dd', 'en');
+    this.user = this.authService.currentUserValue;
+    //TODO change
+    this.today = formatDate(datefns.startOfYesterday(), 'yyyy-MM-dd', 'en');
   }
 
   ngAfterViewInit() {
@@ -64,7 +75,7 @@ export class LabComponent implements OnInit, OnDestroy, AfterViewInit {
           this.searchInitialized = false;
         }
       });
-      this.folderNoControl.setValue('a0000034/20');
+    this.folderNoControl.setValue('a0000034/20');
   }
 
   ngOnDestroy() { }
@@ -82,26 +93,45 @@ export class LabComponent implements OnInit, OnDestroy, AfterViewInit {
     try {
       this.patient = await this.opdService.getPatient(folderNo).toPromise();
       this.consultation = await this.physicianService
-      .getConsultation(this.patient.id, this.today).toPromise();
+        .getConsultation(this.patient.id, this.today).toPromise();
       this.labRequest = await this.labService
-      .getLabInvestigation(this.consultation.id, this.today).toPromise();
-      const params = await this.labSetupService.getLabParams(this.labRequest.service_id)
-      .toPromise();
-      this.labParams = this.processParams(params);
+        .getLabInvestigation(this.consultation.id, this.today).toPromise();
+      if (this.labRequest.status === 'IN-QUEUE') {
+        this.sampleTypes = await this.labSetupService.getLabSampleTypes(this.labRequest.service_id)
+          .toPromise();
+      } if (this.labRequest.status === 'SAMPLE-TAKEN') {
+        this.labService.getSample(this.labRequest)
+          .subscribe(sample => this.sample = sample, error => console.log(error));
+        this.labParams = await this.processParams();
+      } else if (this.labRequest.status === 'RESULTS-TAKEN') {
+        this.results = await this.labService.getLabResults(this.labRequest.id)
+          .toPromise();
+      }
     } catch (e) {
       this.patient = null;
+      this.labParams = null;
       this.labRequest = null;
       this.consultation = null;
       this.message = 'Folder not found';
-    this.searchInitialized = false;
+      this.searchInitialized = false;
     }
     this.isLoadingData = false;
   }
 
-  processParams(params: any[]): any[] {
+  async processParams(): Promise<any[]> {
+    const params = await this.labSetupService.getLabParams(this.labRequest.service_id)
+      .toPromise();
     const temp = [];
     params.forEach((param, index) => {
       param.form = this.fb.control(null);
+      if (param.value_type === 'Text') {
+        param.loading = true;
+        this.labSetupService.getParamFlags(param.id)
+          .subscribe((flags: any[]) => {
+            param.flags = flags.filter(flag => flag.isActivated);
+            param.loading = false;
+          });
+      }
       if (index % 2 === 0) {
         temp.push([param]);
       } else {
@@ -141,11 +171,67 @@ export class LabComponent implements OnInit, OnDestroy, AfterViewInit {
     this.folderNoControl.reset();
   }
 
-  done() {
+  async generateSample() {
+    this.isCreatingSample = true;
+    this.notificationS.blank('Loading', 'Please wait');
+    try {
+      const sampleCode = await this.labService.newSampleCode(this.labRequest.id, this.sampleTypeCtrl.value)
+        .toPromise();
+      this.sample = await this.labService.createSample(this.sampleData(sampleCode))
+        .toPromise();
+      this.labRequest = await this.labService.updateLabRequest(this.labRequest.id, { status: 'SAMPLE-TAKEN' })
+        .toPromise();
+      this.labParams = await this.processParams();
+      this.isCreatingSample = false;
+    } catch (e) {
+      this.notificationS.error('Error', 'Something went wrong');
+    }
   }
 
-  processData() {
-    return {};
+  sampleData(sampleCode: string) {
+    return {
+      sample_code: sampleCode,
+      lab_sample_type_id: this.sampleTypeCtrl.value,
+      investigation_id: this.labRequest.id,
+      technician_id: this.user.details.id,
+    };
+  }
+
+  async done() {
+    try {
+      this.results = await this.labService.saveLabResults(this.processResults()).toPromise();
+      this.labRequest = await this.labService.updateLabRequest(this.labRequest.id, { status: 'RESULTS-TAKEN' })
+        .toPromise();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  processResults() {
+    const data = {
+      investigation_id: this.labRequest.id,
+      results: [],
+      // technician_id: this.user.details.id,
+      patient_status: this.labRequest.patient_status
+    };
+    this.labParams.forEach((params: any[]) => {
+      params.forEach(param => {
+        data.results.push({
+          lab_parameter_id: param.id,
+          test_value: param.form.value
+        });
+      });
+    });
+    return data;
+  }
+
+  resultFormValid() {
+    const valid = this.labParams.reduce((previous: any[], current: any[]) => {
+      const preValid = previous.reduce((prev, cur) => prev.form.valid && cur.form.valid);
+      const curValid = current.reduce((prev, cur) => prev.form.valid && cur.form.valid);
+      return preValid && curValid;
+    });
+    return valid;
   }
 
   attendanceClick(attendance: any) {
--- src/app/laboratory/services/laboratory.service.ts
+++ src/app/laboratory/services/laboratory.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
+import { map, first, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LaboratoryService {
+  readonly investigationUrl = `${environment.apiBaseUrl}/registry/investigations`;
 
   constructor(private http: HttpClient) { }
 
   getLabInvestigation(consultation_id: string, date: string) {
-    const url = `${environment.apiBaseUrl}/registry/investigations`;
-    return this.http.get<any>(url, {
+    return this.http.get<any>(this.investigationUrl, {
       params: {
         'consultation_id': `${consultation_id}`,
         'consultation_date': date,
@@ -24,6 +25,88 @@ export class LaboratoryService {
         }
         throw new HttpErrorResponse({ status: 404 });
       }
-    ));
+    ), first());
+  }
+
+  updateLabRequest(id: number, data: any) {
+    return this.http.put<any>(`${this.investigationUrl}/${id}`, data).pipe(map(
+      res => {
+        if (res && res.data) {
+          return res.data;
+        }
+        throw new HttpErrorResponse({});
+      }
+    ), first());
+  }
+
+  newSampleCode(investigation_id: number, sample_type_id: number) {
+    const url = `${environment.apiBaseUrl}/labs/investigations/` +
+      `${investigation_id}/samplestypes/${sample_type_id}/newcode`;
+    return this.http.get<any>(url).pipe(map(
+      res => {
+        if (res && res.data) {
+          return res.data;
+        }
+        throw new HttpErrorResponse({});
+      }
+    ), first());
+  }
+
+  createSample(data: any) {
+    const url = `${environment.apiBaseUrl}/labs/samples`;
+    return this.http.post<any>(url, data).pipe(map(
+      res => {
+        if (res && res.data) {
+          return res.data;
+        }
+        throw new HttpErrorResponse({});
+      }
+    ), first());
+  }
+
+  getSample(investigation: any) {
+    const url = `${environment.apiBaseUrl}/labs/samples`;
+    return this.http.get<any>(url, {
+      params: {
+        investigation_id: investigation.id,
+        service_id: investigation.service_id
+      }
+    }).pipe(map(
+      res => {
+        if (res && res.data && res.data.length > 0) {
+          return res.data[0];
+        }
+        throw new HttpErrorResponse({});
+      }
+    ), first());
+  }
+
+  saveLabResults(result: any) {
+    const url = `${environment.apiBaseUrl}/labs/results/multiple`;
+    return this.http.post<any>(url, result).pipe(map(
+      res => {
+        if (res && res.data) {
+          return res.data;
+        }
+        throw new HttpErrorResponse({});
+      }
+    ), first());
+  }
+
+  getLabResults(investigation_id: number) {
+    const url = `${environment.apiBaseUrl}/labs/results`;
+    return this.http.get<any>(url, {
+      params: {
+        investigation_id: `${investigation_id}`
+      }
+    }).pipe(map(
+      res => {
+        if (res && res.data) {
+          return res.data;
+        } else {
+          return [];
+        }
+      }
+    ), first(), catchError(e => of([])));
   }
 }
