35b1006fd65829d2ccb149d6e005dc847f144a88
Joseph Cobbinah <josephcobbinah.jc@gmail.com>
2020-04-29 16:04:47 +0000

    styled dashboard stats items

--- src/app/dashboard/default/default-dashboard.component.ts
+++ src/app/dashboard/default/default-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './default-dashboard.component.html'
+  templateUrl: './default-dashboard.component.html',
+  styleUrls: ['./default-dashboard.component.scss']
 })
-export class DefaultDashboardComponent {}
+export class DefaultDashboardComponent { }
